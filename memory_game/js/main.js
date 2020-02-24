2c56f5fb0bad61dfcb220e16966f3787af894fb5
Nausheen Salauddin <nausheen@nausheens-mbp.lan>
2020-02-24 13:03:34 +0800

    added game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,17 @@
 console.log("Up and running!");
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
-console.log("User flipped" + cardOne);
-console.log("User flipped" + cardThree);
+let cards = ["queen","queen","king","king"];
+let cardsInPlay = [];
+let cardOne = cards[0];
+let cardTwo = cards[2];
+cardsInPlay = cards.push(cardOne);
+cardsInPlay = cards.push(cardTwo);
+if(cardsInPlay.length === 2){
+	if(cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match!");
+	}
+	else{
+		alert("Sorry, try again.");
+	}
+}
+console.log("user flipped" + " " + cardOne);
+console.log("user flipped" + " " + cardTwo);
