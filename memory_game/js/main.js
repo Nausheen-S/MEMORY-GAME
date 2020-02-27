2028462114aabc197642edb4547fa38e93193e06
Nausheen Salauddin <nausheen@nausheens-mbp.lan>
2020-02-27 13:16:11 +0800

    created working game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -30,14 +30,29 @@ function checkForMatch(){
   alert("Sorry, try again.");
 }
 }
-function flipCard(cardId) {
+
+function createBoard(){
+	for(let i=0;i<cards.length;i++){
+		let cardElement = document.createElement('img');
+		cardElement.setAttribute('src','images/back.png');
+		cardElement.setAttribute('data-id',i);
+		cardElement.addEventListener('click',flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+
+	}
+}
+
+
+
+function flipCard() {
+	var cardId = this.getAttribute('data-id');
 	console.log("user flipped" + " " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src',cards[cardId].cardImage)
 if(cardsInPlay.length === 2){
 	checkForMatch();
 }
 }
-flipCard(0);
-flipCard(2);
+createBoard();
